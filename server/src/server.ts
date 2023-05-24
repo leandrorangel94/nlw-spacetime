a3ffbbafb0e44a4704c6781a8cac4a52f9524466
Leandro Rangel <rangel94@outlook.com>
2023-05-24 10:57:59 -0300

    aula 2: modificações back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany();
-  return users;
+app.register(cors, {
+  origin: true, //Aceita requisições de toda URL front-end
 });
 
+app.register(memoriesRoutes);
+
 app.listen({ port: 3333 }).then(() => {
-  console.log("🚀 HTTP server running on http://localhost/3333!");
+  console.log("🚀 HTTP server running on http://localhost:3333/");
 });
