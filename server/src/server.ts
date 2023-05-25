342f0b628ec2359199abbc45932523b3895da806
Leandro Rangel <rangel94@outlook.com>
2023-05-25 11:05:24 -0300

    aula 3: Configuração Github OAuth no Frontend e autenticação JWT no Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import jwt from "@fastify/jwt";
 
 const app = fastify();
 
@@ -8,6 +12,11 @@ app.register(cors, {
   origin: true, //Aceita requisições de toda URL front-end
 });
 
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.listen({ port: 3333 }).then(() => {
